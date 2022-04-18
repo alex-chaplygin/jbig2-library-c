88ac42071b04e88b49369ec652d4f135c1f684b6
alex-chaplygin <alex_chaplygin@mail.ru>
2022-04-18 07:25:00 +0300

    Добавлена структура точки

--- bitmap.h
+++ bitmap.h
@@ -1,8 +1,16 @@
 #include "types.h"
 
+/// точка
+typedef struct {
+  int x;			/**< координата x */
+  int y;			/**< координата y */
+} point_t;
+
 /// битовая карта
 typedef struct {
     int width;			/**< ширина */
     int height;			/**< высота */
     byte *data;			/**< массив пикселей по строкам сверху-вниз справо-налево */
 } bitmap_t;
+
+bitmap_t *new_bitmap(int w, int h); // создать новую карту
