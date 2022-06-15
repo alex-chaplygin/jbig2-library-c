84ccb31cd87d4de67db7d51068d51d81b2fb977a
alex-chaplygin <alex_chaplygin@mail.ru>
2022-06-15 11:58:39 +0300

    Тестирование обновленного региона

--- test/test_symbol_dict.c
+++ test/test_symbol_dict.c
@@ -1,6 +1,64 @@
 #include <stdio.h>
 #include "symbol_dict.h"
 
+int values_array[] = { 10, 2, 4, 5, 3, 7, 9, 6 }; // массив значений delta height и delta width
+int height_class_change_indexes[] = { 2, 5 }; // индексы массива values_array, на которых меняется класс высот
+int change_indexes_length = sizeof(height_class_change_indexes) / sizeof(int);
+int current_class_index = 0;
+int class_counter = 0;
+int last_oob = 0;
+
+int huff_decode(int *val, int *code_size) 
+{
+    *code_size = 1;
+    
+    if (current_class_index < change_indexes_length
+        && class_counter == height_class_change_indexes[current_class_index] 
+        && last_oob == 1) 
+    {
+        last_oob = 0;
+        if (current_class_index < change_indexes_length)
+            current_class_index++;
+    } 
+    else if (current_class_index < change_indexes_length
+        && class_counter == height_class_change_indexes[current_class_index] 
+        || class_counter >= sizeof(values_array) / sizeof(int)) 
+    {
+        *val = 0;
+        last_oob = 1;
+        return last_oob;
+    }
+    
+    *val = values_array[class_counter++];
+    return last_oob;
+}
+
+void huff_set_pref_table(byte *table) 
+{
+    return;
+}
+
+void arith_int_decode(int *v, int *s) 
+{
+    if (current_class_index < change_indexes_length
+        && class_counter == height_class_change_indexes[current_class_index] 
+        && *s == 1) 
+    {
+        *s = 0;
+        if (current_class_index < change_indexes_length)
+            current_class_index++;
+    } 
+    else if (current_class_index < change_indexes_length
+        && class_counter == height_class_change_indexes[current_class_index] 
+        || class_counter >= sizeof(values_array) / sizeof(int)) 
+    {
+        *v = 0;
+        *s = 1;
+        return;
+    }    
+    *v = values_array[class_counter++];
+}
+
 void bitmap_crop(bitmap_t *src, bitmap_t *dst, int start_col, int width) 
 {
     dst->width = width;
@@ -9,8 +67,8 @@ void bitmap_crop(bitmap_t *src, bitmap_t *dst, int start_col, int width)
 
 int main()
 {
-    int huff = 1;
-	int refine_agg = 0; 
+    int huff = 0;
+    int refine_agg = 1; 
 	uint num_input_syms = 5; 
 	bitmap_t input_syms[5]; 
 	uint num_new_syms = 5; 
